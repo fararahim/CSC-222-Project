b731c70ec81e171fb6a98e5af862c10b62944c19
FayfouzZeidann <47015603+FayfouzZeidann@users.noreply.github.com>
2024-10-27 11:05:35 -0500

    Add files via upload
    uh

---
+++ tech_shell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+//the main program loop
+void program_loop(){
+}
+
+//reads in input
+void read(char* str){
+
+}
+
+//parses input line
+char **parse(char *line){
+
+}
+
+
+//may end up using execvp. will check back on this one
+int execute(char **args){
+
+}
+
+
+int main(int argc, char *argv){
+
+program_loop();
+
+return EXIT;
+}
